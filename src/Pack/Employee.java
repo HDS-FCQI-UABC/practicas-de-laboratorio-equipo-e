c32e5e2866e87524de13302dd082b92789c2261d
Erick Cano <ericke.cano@gmail.com>
2022-08-31 19:36:14 -0700

    Agregue las clases Employee y Resource

---
+++ src/Pack/Employee.java
@@ -0,0 +1,17 @@
+package Pack;
+
+public class Employee {
+
+	private String name;
+
+	public Employee() {
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/Pack/Resource.java
@@ -0,0 +1,17 @@
+package Pack;
+
+public class Resource {
+
+	private String name;
+
+	public Resource() {
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
